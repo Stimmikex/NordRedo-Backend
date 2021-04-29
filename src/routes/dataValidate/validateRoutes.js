3454349f2b24b8fb15a60abc144e837acf182c92
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-04-29 21:39:40 +0000

    temp remove of regex matching

--- src/routes/dataValidate/validateRoutes.js
+++ src/routes/dataValidate/validateRoutes.js
@@ -19,15 +19,15 @@ export const eventRules = () => [
     .isInt()
     .isLength({ min: 0, max: 420 })
     .withMessage('Seats is required, max 420 integer number'),
-  body('date')
-    .isDate()
-    .withMessage('date must be a date'),
-  body('startDate')
-    .matches('/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]/')
-    .withMessage('not dateTime format'),
-  body('endDate')
-    .matches('/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]/')
-    .withMessage('not dateTime format'),
+  // body('date')
+  //   .isDate()
+  //   .withMessage('date must be a date'),
+  // body('startDate')
+  //   .matches('/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]/')
+  //   .withMessage('not dateTime format'),
+  // body('endDate')
+  //   .matches('/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]/')
+  //   .withMessage('not dateTime format'),
   body('location')
     .isString(),
   body('event_type_id')
