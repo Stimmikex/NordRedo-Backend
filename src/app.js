23c10ce4f83854f7bcff62036cff84d542e9c684
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-05-07 12:36:07 +0000

    Im losing my sight

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 
 app.use((_req, res, next) => {
   res.header(
-    'Access-Control-Allow-Origin', 'https://nordredo-backend.herokuapp.com/',
+    'Access-Control-Allow-Origin', '*',
   );
   res.header(
     'Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH',
