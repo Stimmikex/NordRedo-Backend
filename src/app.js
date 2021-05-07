ff26d060da67bfbbf5ed310714f6b2bc7f7a4d8c
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-05-07 13:11:09 +0000

    why!

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use((_req, res, next) => {
     'Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH',
   );
   res.header(
-    "Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+    "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   );
   res.header(
     'Access-Control-Allow-Credentials', 'true',
