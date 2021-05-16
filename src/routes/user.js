900f3a8a462d34d19051543640f3a8e23abfd448
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-05-16 11:23:55 +0000

    moving login, register and me to routes user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,6 +9,117 @@ dotenv.config();
 
 export const routerUser = express.Router();
 
+routerUser.post('/register',
+  body('username')
+    .trim()
+    .isLength({ min: 1, max: 256 })
+    .withMessage('username is required, max 256 characters')
+    .custom((value) => {
+      return users.getUserByUsername(value).then(user => {
+        if(user) {
+          return Promise.reject('username already exists');
+        }
+      });
+    }),
+  body('password')
+    .trim()
+    .isLength({ min: 10, max: 256 })
+    .withMessage('Password is required, min 10 characters, max 256 characters'),
+  async (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+    
+    const token = createTokenForUser(user.id);
+
+    const createdUser = await users.createUser(username, password, token);
+
+    console.log(createdUser);
+
+    if (createdUser) {
+      return res.json({
+        id: createdUser.id, 
+        username: createdUser.name,
+        token,
+      });
+    }
+
+    return res.json({ error: 'Error registering' });
+});
+
+routerUser.post('/login', 
+  body('username')
+    .trim()
+    .isLength({ min: 1, max: 256 })
+    .withMessage('username is required, max 256 characters'),
+  body('password')
+    .trim()
+    .isLength({ min: 10, max: 256 })
+    .withMessage('password is required, min 10 characters, max 256 characters'),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+
+    const user = await users.getUserByUsername(username);
+
+    if (!user) {
+      return res.status(401).json({ errors: [{
+        value: username,
+        msg: "username or password incorrect",
+        param: 'username', 
+        location: 'body'
+      }]});
+    }
+
+    const passwordIsCorrect = users.comparePasswords(password, user.password);
+
+    if (passwordIsCorrect) {
+      const token = createTokenForUser(user);
+      res.setHeader('Set-Cookie', cookie.serialize('auth', token, {
+        httpOnly: false,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 3600,
+        path: '/'
+      }))
+      await users.updateUserTokenById(req.cookies.auth, user.id);
+      return res.json({
+        "user": {
+          id: user.id,
+          username: user.username,
+          role_id: user.role_id
+        },
+        token,
+        expiresIn: "not implemented",
+    });
+  }
+
+  return res.status(401).json({ errors: [{
+    value: username,
+    msg: "username or password incorrect",
+    param: 'username', 
+    location: 'body'
+  }]});
+});
+
+routerUser.get('/me',
+  requireAuthentication,
+  (req, res) => {
+    const user = req.user 
+    return res.json({
+      user,
+  });
+});
+
 routerUser.patch('/me', 
 requireAuthentication,
   body('password')
