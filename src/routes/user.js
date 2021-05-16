e3fe69c6241bee41725a171b9e6cfb20fef1f3cc
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2021-05-16 14:49:49 +0000

    fixed createTokenForUser reference

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import express from "express";
 import * as users from "../dataOut/users.js";
 import { body, param, validationResult } from "express-validator";
-import { requireAuthentication, requireAdminAuthentication } from "../dataOut/login.js";
+import { createTokenForUser, requireAuthentication, requireAdminAuthentication } from "../dataOut/login.js";
 
 dotenv.config();
 
