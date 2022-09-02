e88cd8a2a5bcb63782863ae667db83c324c74b72
Styrmir Óli Þorsteinsson <styrmir97@gmail.com>
2022-09-02 13:22:11 +0000

    study changes and making sql for search by name

--- src/dataOut/items.js
+++ src/dataOut/items.js
@@ -11,3 +11,17 @@ export async function getItems() {
     }
     return null;
   }
+
+  export async function getItemByName(name) {
+    const q = `SELECT items.name, price, text, image, quantity, discount, username, item_types.name as typename FROM items
+                INNER JOIN users ON users.id = items.user_id 
+                INNER JOIN item_types ON item_types.id = items.type_id
+              WHERE items.name = $1`;
+    try {
+      const result = await query(q, [name]);
+      return result.rows[0];
+    } catch (e) {
+      console.error('There is no event with this id');
+    }
+    return null;
+  }
--- src/routes/store.js
+++ src/routes/store.js
@@ -1,8 +1,20 @@
 import express from 'express';
-import { getItems } from '../dataOut/items.js';
+import { getItems, getItemByName } from '../dataOut/items.js';
+import { checkValidationResult, eventRules, paramIdRules, patchEventRules } from './dataValidate/validateRoutes.js';
+
+import { requireAdminAuthentication, requireAuthentication } from '../dataOut/login.js'
+
 export let routerStore = express.Router();
 
 routerStore.get('/', async (req, res) => {
     const items = await getItems();
     res.json(items);
+});
+
+routerStore.get('/:itemName',
+    paramIdRules(),
+    async (req, res) => {
+    const name = req.params.itemName;
+    const item = await getItemByName(name);
+    res.json(item);
 });
